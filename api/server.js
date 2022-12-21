90c259fe49f69344436985437ec3cc128cc0cefa
Phat Panhareach <111555521+Re4ch-Jay@users.noreply.github.com>
2022-12-21 20:12:42 +0700

    conn db

--- api/server.js
+++ api/server.js
@@ -1,7 +1,18 @@
+require('dotenv').config()
 const express = require("express")
+const mongoose = require("mongoose")
 const app = express();
 
+mongoose.set('strictQuery', false);
+// connect to db
+mongoose.connect(process.env.MONGO_URL)
+    .then(res => {
+        app.listen(process.env.PORT, () => {
+            console.log('Server is running on Port ' + process.env.PORT + ' connected to db')
+        })
+    })
+    .catch(err => {
+        console.log(err)
+    })
 
-app.listen(4000, () => {
-    console.log('Server is running')
-})
+
