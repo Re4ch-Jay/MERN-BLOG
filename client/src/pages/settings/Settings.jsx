39ac3169c4977593252814c7d5053f88b7101ff2
Phat Panhareach <111555521+Re4ch-Jay@users.noreply.github.com>
2022-12-28 23:01:03 +0700

    update user

--- client/src/pages/settings/Settings.jsx
+++ client/src/pages/settings/Settings.jsx
@@ -1,13 +1,56 @@
+import axios from 'axios'
+import { useContext, useEffect, useState } from 'react'
 import Sidebar from '../../components/sidebar/Sidebar'
 import './settings.css'
+import {Context} from "../../context/Context"
+import { useNavigate } from 'react-router-dom'
 
 function Settings() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  
+  const {user, dispatch} = useContext(Context)
+  const navigate = useNavigate()
+
+  const handleDelete = async () => {
+    const res = await axios.delete(`/user/${user._id}`, {data: {userId: user._id}});
+    if(res) {
+      dispatch({type: "LOGOUT"})
+      navigate('/')
+    }
+  }
+
+  const handleUpdate = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.put(`/user/${user._id}`, {userId: user._id, username, email, password})
+      console.log(res)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const res = await axios.get(`/user/${user._id}`)
+        console.log(res.data)
+        setUsername(res.data.username);
+        setEmail(res.data.email);
+        setPassword(res.data.password);
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    fetchUser()
+  }, [])
+
   return (
     <div className='settings'>
       <div className="settingWrapper">
         <div className="settingTitle">
           <span className="settingUpdateTitle">Update Your Account</span>
-          <span className="settingDeleteTitle">Delete Account</span>
+          <span className="settingDeleteTitle" onClick={handleDelete}>Delete Account</span>
         </div>
         <form className='settingForm'>
           <label>Profile Picture</label>
@@ -17,16 +60,16 @@ function Settings() {
                 src="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
                 alt=""
               />
-              <label htmlFor="fileInput"><i class="settingProfilePictureIcon fa-solid fa-user"></i></label>
+              <label htmlFor="fileInput"><i className="settingProfilePictureIcon fa-solid fa-user"></i></label>
               <input type="file" id="fileInput" style={{display: 'none'}} />
           </div>
           <label>Username</label>
-          <input type="text" placeholder='John Doe' />
-          <label>Username</label>
-          <input type="email" placeholder='johndoe@gmail.com' />
+          <input type="text" placeholder='John Doe' value={username} onChange={e => setUsername(e.target.value)}/>
+          <label>Email</label>
+          <input type="email" placeholder='johndoe@gmail.com' value={email} onChange={e => setEmail(e.target.value)} />
           <label>Password</label>
-          <input type="password" />
-          <button className='settingSubmit'>Update Profile</button>
+          <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+          <button className='settingSubmit' type='submit' onClick={handleUpdate}>Update Profile</button>
         </form>
       </div>
       <Sidebar />
